341161eb5341e7028769cc9c8e512b074a2c1956
Filimon Mikhail <misafilimon255@mail.ru>
2020-06-17 16:52:42 +0300

    fixed(js/script.js)add function cansel game

--- js/script.js
+++ js/script.js
@@ -26,18 +26,14 @@ function replyBot(n) {
     } else if(!isNumber(reply_user)) {
         alert('Введи число!');
         replyBot(n);
-    } else if (n > reply_user){
+    } else if (n > reply_user && reply_user != ''){
         alert('Загаданное число больше');
         replyBot(n);
-    } else if (n < reply_user){
+    } else if (n < reply_user && reply_user != ''){
         alert('Загаданное число меньше');
-        replyBot(n);
-    } else {
         replyBot(n);
     }
 }
 replyBot(num);
 
-// Я не понял как сделать отмену, я перепробовал варианты типа (reply_user == null) {alert('Отмена игры')}
-// Но не помогло, можете мне помочьи обьяснить как это сделать, а также помочь решить проблему с Es6 и Es7
 
